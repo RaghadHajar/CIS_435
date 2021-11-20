4cf081940c2141193de23bc573508f0e74e91bce
Jason Bean <jasonb0820@gmail.com>
2021-11-20 09:59:18 -0500

    Edit to file structure again

---
+++ Back_end/JS/P3.js
@@ -0,0 +1,14 @@
+//this is where I would put code... IF I HAD ANY!!
+
+//lecture 8 1:36:00 goes over how to use fs 
+//which is a node module that allows for reading/writing to files
+//Hopefully will be able to use this to read the users.JSON file
+//also pour one out for Fudgeloptopus :'(
+
+//return ture if user exists
+//help from lec 8 57:00, he is doing it with AJAX tho
+
+//lets try implementing what he did
+const express = require('express');
+const app = express();
+const cors = require('cors');
